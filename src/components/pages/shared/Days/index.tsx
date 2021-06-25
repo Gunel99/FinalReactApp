b68f4dfd58626eaaa4a1eb1fd7e2549b4b335d58
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2021-06-25 09:05:32 +0400

    Added redux

--- src/components/pages/shared/Days/index.tsx
+++ src/components/pages/shared/Days/index.tsx
@@ -2,7 +2,6 @@ import { FC, ReactElement } from 'react';
 import './style.scss';
 import { IDaysProps } from './types';
 
-
 const Days: FC<IDaysProps> = ({ day }): any => {
     return (
         <div className='day'>
